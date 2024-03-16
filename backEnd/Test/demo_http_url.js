e8dbf0f152e6ea1d2016522ed7462f3d380608c4
Aksjaj Mehilal <113827304+AksjajM@users.noreply.github.com>
2024-03-16 20:26:16 +0100

    changes

--- backEnd/Test/demo_http_url.js
+++
@@ -1,8 +0,0 @@
- var http = require('http'); 
-  
-var app = http.createServer(function(req,res){ 
-    res.setHeader('Content-Type', 'application/json'); 
-    res.end(JSON.stringify({ number: 1 , name: 'John'})); 
-}); 
-  
-app.listen(3000);
